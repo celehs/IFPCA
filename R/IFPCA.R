72b6a3740bb3bce9331cfd58d76e019f452d86e2
Ming Yang <hustyangming@gmail.com>
2020-09-14 17:16:24 +0000

    2020-09-14

--- R/IFPCA.R
+++ R/IFPCA.R
@@ -13,22 +13,23 @@ ifpca <- function(time, fu_train, fu_valid,
   fu <- c(fu_train, fu_valid)
   time_std <- time / fu[names(time)]   
   train <- names(fu_train) 
-  valid <- names(fu_valid)  
+  valid <- names(fu_valid) 
   count_train <- count[names(count) %in% train]
   count_valid <- count[names(count) %in% valid]
   count_train_all <- 0 * fu_train
   count_valid_all <- 0 * fu_valid
   count_train_all[names(count_train)] <- count_train  
   count_valid_all[names(count_valid)] <- count_valid
-  # training
+  # TRAINING
   time_train <- time[names(time) %in% train]
   PKTS <- GetPK(
     id = as.integer(names(time_train)), ### INT/CHAR ### 
     t = time_train, 
-    tseq = 0:floor(max(fu)), 
+    tseq = 0:floor(max(fu_train)), 
     nn = length(fu_train))
   time_std_train <- time_std[names(time_std) %in% train] 
-  time1_train <- tapply(time_train, as.integer(names(time_train)), min)  # names(time_train)
+  time1_train <- tapply(time_train, 
+                        as.integer(names(time_train)), min)  # names(time_train)
   Tend <- 1
   h1 <- bw.nrd(time_std_train)
   h2 <- bw.nrd(time_std_train)^(5/6)
@@ -65,5 +66,44 @@ ifpca <- function(time, fu_train, fu_valid,
     mean = tmp$mean, 
     basis = tmp$basis, 
     baseline = tmp$baseline)
-  return(list(FPCA = FPCA))
+  colnames(ft.e) <- c("1stCode", "Pk", "ChP", "1stScore", "logN")
+  colnames(ft.e.S) <- c("1stCode", "1stScore", "2ndScore", "3rdScore", "4thScore", "logN")
+  rownames(ft.e.S) <- rownames(ft.e) <- names(PKTS) <- train
+  # VALIDATION
+  time_valid <- time[names(time) %in% valid]
+  PKTS2 <- GetPK(
+    id = as.integer(names(time_valid)), ### INT/CHAR ### 
+    t = time_valid, 
+    tseq = 0:floor(max(fu_valid)), 
+    nn = length(fu_valid))  
+  time_std_valid <- time_std[names(time_std) %in% valid]   
+  time1_valid <- tapply(time_valid, 
+                        as.integer(names(time_valid)), min)  # names(time_train)
+  tmp <- PP.FPCA.Pred(time_std_valid, count_valid, FPCA$mean, FPCA$basis, FPCA$K)
+  FPCA$ValidPred <- tmp
+  ft.e2 <- cbind(
+    matrix(fu_valid, nrow = length(fu_valid), ncol = 3), 
+    -tmp$baseline[1], log(1 + count_valid_all))
+  pos <- count_valid_all > 0  
+  locm <- unlist(apply(tmp$densities[, 1:sum(pos) + 1], 2, which.max))  
+  ft.e2[pos, 2] <- ft.e2[pos, 2] * tmp$densities[locm, 1]
+  ft.e2[pos, 3] <- ft.e2[pos, 3] * tmp$derivatives[
+    sapply(1:sum(pos), function(i) {
+      which.max(tmp$derivatives[1:locm[i], i + 1])
+    }), 1]
+  ft.e2[pos, 4] <- tmp$scores[, 2]   
+  ft.e.S2 <- cbind(
+    ft.e2[, 1], VTM(-tmp$baseline[1:4], length(fu_valid)), log(1 + count_valid_all))
+  ft.e.S2[pos, 2:5] <- as.matrix(tmp$scores[, 2:5])
+  colnames(ft.e2) <- c("1stCode", "Pk", "ChP", "1stScore", "logN")
+  colnames(ft.e.S2) <- c("1stCode", "1stScore", "2ndScore", "3rdScore", "4thScore", "logN")
+  rownames(ft.e.S2) <- rownames(ft.e2) <- names(PKTS2) <- valid  
+  list(
+    TrainFt = ft.e,
+    TrainSc = ft.e.S,
+    ValidFt = ft.e2,
+    ValidSc = ft.e.S2,
+    TrainPK = PKTS,
+    ValidPK = PKTS2,
+    FPCA = FPCA)
 }
